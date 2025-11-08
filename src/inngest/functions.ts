ed2cdb616240010473998852470028010a7ea3bc
bm-197 <bisratmaru111@gmail.com>
2025-11-08 10:11:39 +0300

    feat: add e2b sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,17 @@
+import { Sandbox } from 'e2b';
 import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { getSandbox } from './utils';
 
 
 export const helloworld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("co-dev-test2");
+      return sandbox.sandboxId;
+    })
     const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets",
@@ -16,8 +22,13 @@ export const helloworld = inngest.createFunction(
       `Summarize the following test: ${event.data.value}`
     )
 
-    console.log(output)
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000)
 
-    return { output };
+      return `https://${host}`
+    })
+
+    return { output, sandboxUrl };
   }
 )
