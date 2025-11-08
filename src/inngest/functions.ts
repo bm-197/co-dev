b1069f7f11809dcb63b0f6d1a7b4e7f47be47d29
bm-197 <bisratmaru111@gmail.com>
2025-11-08 08:34:38 +0300

    draft: start the ai agent integration

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,23 @@
+import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+
 
 export const helloworld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "30s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets",
+      model: openai({ model: "gpt-4o" })
+    });
+
+    const { output } = await codeAgent.run(
+      `Summarize the following test: ${event.data.value}`
+    )
+
+    console.log(output)
+
+    return { output };
   }
 )
